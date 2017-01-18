03e639666d0c2033c9226f3188d99dfc0ec389e3
Hunor Kovacs <hunor.kovacs@ft.com>
2017-01-18 19:37:53 +0200

    Another way to connect and print a website. No error at the end at least.

--- build.sbt
+++ build.sbt
@@ -6,8 +6,8 @@ scalaVersion := "2.12.1"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.16",
-  "com.typesafe.akka" %% "akka-http-core" % "10.0.1",
-  "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http-core" % "10.0.2",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.2",
   "org.slf4j" % "slf4j-api" % "1.7.22",
   "ch.qos.logback" % "logback-classic" % "1.1.8"
   )
--- src/main/scala/com/ft/notificationsmonitor/Hello.scala
+++ src/main/scala/com/ft/notificationsmonitor/Hello.scala
@@ -3,41 +3,36 @@ package com.ft.notificationsmonitor
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
-import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import akka.util.ByteString
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.Await
-import scala.util.{Failure, Success}
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object Hello extends App {
 
   private val logger = LoggerFactory.getLogger(getClass)
   implicit private val sys = ActorSystem("notifications-monitor")
-  implicit private val mat = ActorMaterializer()
+  implicit private val mat = ActorMaterializer(ActorMaterializerSettings(sys).withDebugLogging(true))
+
   import sys.dispatcher
 
-  private val http = Http()
-  private val reqF = http.singleRequest(HttpRequest(uri = "http://akka.io"))
+  val connectionFlow = Http().outgoingConnectionHttps("getbootstrap.com")
+  val responseFuture = Source.single(HttpRequest(uri = "/"))
+    .via(connectionFlow)
+    .runWith(Sink.head)
 
-  http.singleRequest(HttpRequest(uri = "http://akka.io")).onComplete {
-      case Success(resp) =>
-        resp match {
-          case HttpResponse(StatusCodes.OK, headers, entity, _) =>
-            entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
-              logger.info("Got response, body: " + body.utf8String)
-            }
-          case resp @ HttpResponse(code, _, _, _) =>
-            logger.info("Request failed, response code: " + code)
-            resp.discardEntityBytes()
-        }
-      case Failure(t) =>
-        logger.warn("fail")
-    }
-
-  Await.ready(
-    reqF.flatMap(_ => sys.terminate().
-      map(_ => logger.info("Bye, world!"))
-    ), 2 seconds)
+  responseFuture.andThen {
+    case Success(resp) =>
+      Await.ready(resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(body => logger.info(body.utf8String)), 10 seconds)
+    case Failure(_) => logger.warn("request failed")
+  }.andThen {
+    case _ =>
+      Await.ready(Http().shutdownAllConnectionPools()
+        .flatMap(_ => sys.terminate()), 5 seconds)
+      logger.info("terminated")
+  }
 }
