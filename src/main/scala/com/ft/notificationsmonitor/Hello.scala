c49100761526ad84bd772ed4cc6735ef76bf138a
Hunor Kovacs <hunor.kovacs@ft.com>
2017-01-19 18:21:47 +0200

    Nicely closing HTTP body stream and exiting program on SIGINT or other shutdown. Printing output.

--- src/main/scala/com/ft/notificationsmonitor/Hello.scala
+++ src/main/scala/com/ft/notificationsmonitor/Hello.scala
@@ -2,17 +2,18 @@ package com.ft.notificationsmonitor
 
 import java.io.File
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpHeader, HttpRequest, HttpResponse, StatusCodes}
-import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, KillSwitches}
 import akka.util.ByteString
 import org.slf4j.LoggerFactory
 import akka.http.scaladsl.model.headers._
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Promise}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
@@ -24,28 +25,52 @@ object Hello extends App {
 
   import sys.dispatcher
 
-  val sensitiveConfig = ConfigFactory.parseFile(new File("src/main/resources/.sensitive.conf"))
+  private val sensitiveConfig = ConfigFactory.parseFile(new File("src/main/resources/.sensitive.conf"))
   ConfigFactory.load().withFallback(sensitiveConfig)
-  val username = sensitiveConfig.getString("basic-auth.username")
-  val password = sensitiveConfig.getString("basic-auth.password")
 
-  logger.info(username)
-  logger.info(password)
+  scala.sys addShutdownHook shutdown
+
+  private val killPromise = Promise[Done]()
 
   val connectionFlow = Http().outgoingConnectionHttps("pre-prod-uk-up.ft.com")
-  val request = HttpRequest(uri = "/content/notifications-push").addHeader(Authorization(BasicHttpCredentials(username, password)))
-  val responseFuture = Source.single(request)
+  val request = HttpRequest(uri = "/content/notifications-push")
+    .addHeader(Authorization(BasicHttpCredentials(
+      sensitiveConfig.getString("basic-auth.username"),
+      sensitiveConfig.getString("basic-auth.password")
+    )))
+  val responseF = Source.single(request)
     .via(connectionFlow)
     .runWith(Sink.head)
-  responseFuture.andThen {
-    case Success(resp) =>
-      logger.info(resp.status.value)
-      Await.ready(resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(body => logger.info(body.utf8String)), 10 seconds)
-    case Failure(_) => logger.warn("request failed")
-  }.andThen {
-    case _ =>
-      Await.ready(Http().shutdownAllConnectionPools()
-        .flatMap(_ => sys.terminate()), 5 seconds)
-      logger.info("terminated")
+  responseF onComplete {
+    case Failure(exception) => logger.warn("Failed request", exception)
+    case Success(response) =>
+      logger.info(response.status.value)
+      val killSwitch = consumeBodyByPrinting(response.entity.dataBytes)
+      killPromise.future.onComplete(_ => killSwitch.shutdown())
+  }
+
+  private def consumeBodyByPrinting(dataBytes: Source[ByteString, Any]) = {
+    dataBytes
+      .viaMat(KillSwitches.single)(Keep.right)
+      .fold(ByteString("")){ (acc, next) =>
+        if (next.equals(ByteString('\n'))) {
+          logger.info(acc.utf8String)
+          ByteString("")
+        } else {
+          System.out.print((next ++ ByteString("|")).utf8String)
+          acc ++ next
+        }
+      }
+      .toMat(Sink.ignore)(Keep.left)
+      .run()
+  }
+
+  private def shutdown() = {
+    logger.info("Exiting...")
+    killPromise.complete(Success(Done))
+    Await.ready(
+      Http().shutdownAllConnectionPools()
+        .flatMap(_ => sys.terminate()), 5 seconds
+    )
   }
 }
