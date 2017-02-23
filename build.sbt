45275ecddaf53aaa3a970910a9d4d7a6f731126a
Hunor Kovacs <hunor.kovacs@ft.com>
2017-02-23 16:27:31 +0200

    Tried adding tests for PullConnector. Refactored into actor and PullHttp class. Added test dependencies.

--- build.sbt
+++ build.sbt
@@ -9,6 +9,9 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http-core" % "10.0.1",
   "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.1",
   "com.typesafe.akka" %% "akka-slf4j" % "2.4.16",
+  "com.typesafe.akka" %% "akka-testkit" % "2.4.16" % "test",
+  "junit" % "junit" % "4.12" % "test",
+  "org.mockito" % "mockito-core" % "2.7.11" % "test",
   "org.slf4j" % "slf4j-api" % "1.7.22",
   "ch.qos.logback" % "logback-classic" % "1.1.8"
   )
---
+++ src/main/java/com/ft/notificationsmonitor/PullHttp.java
@@ -0,0 +1,62 @@
+package com.ft.notificationsmonitor;
+
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.OutgoingConnection;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.headers.Authorization;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import com.ft.notificationsmonitor.model.HttpConfig;
+import com.ft.notificationsmonitor.model.NotificationFormats;
+import com.ft.notificationsmonitor.model.PullPage;
+import spray.json.JsValue;
+import spray.json.JsonParser;
+import spray.json.ParserInput;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.model.StatusCodes.OK;
+
+public class PullHttp {
+
+    private Materializer mat;
+
+    private Flow<HttpRequest, HttpResponse, CompletionStage<OutgoingConnection>> connectionFlow;
+    private HttpConfig httpConfig;
+
+    public PullHttp(ActorSystem sys, HttpConfig httpConfig) {
+        this.httpConfig = httpConfig;
+        mat = ActorMaterializer.create(sys);
+        connectionFlow = Http.get(sys).outgoingConnection(ConnectHttp.toHostHttps(httpConfig.getHostname(), httpConfig.getPort()));
+    }
+
+    public CompletionStage<PullPage> makeRequest(ZonedDateTime date) {
+        HttpRequest request = HttpRequest.create(httpConfig.getUri() + "?since=" + date.format(DateTimeFormatter.ISO_INSTANT))
+                .addHeader(Authorization.basic(httpConfig.getUsername(), httpConfig.getPassword()));
+        return Source.single(request)
+                .via(connectionFlow)
+                .runWith(Sink.head(), mat)
+                .thenCompose(response -> {
+                    if (!response.status().equals(OK)) {
+                        return CompletableFuture.supplyAsync(() -> {
+                           throw new RuntimeException(String.format("Response status not ok on notifications pull request. status=%d", response.status().intValue()));
+                        });
+                    } else {
+                        return response.entity().toStrict(5000, mat)
+                                .thenApply(entity -> entity.getData().utf8String());
+                    }
+                }).thenApply(responseBody -> {
+                    JsValue parser = JsonParser.apply(new ParserInput.StringBasedParserInput(responseBody));
+                    return parser.convertTo(NotificationFormats.pullPageFormat());
+                });
+    }
+}
